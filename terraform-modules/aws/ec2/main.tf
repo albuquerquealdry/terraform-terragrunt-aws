9559382334124eaba8194c178aec6cac1f2c0a57
Aldry Albuquerque <albuquerquealdry@gmail.com>
2022-11-13 17:45:41 -0300

    add. add ec2

---
+++ terraform-modules/aws/ec2/main.tf
@@ -0,0 +1,69 @@
+
+provider "aws" {
+  region = local.region
+}
+
+locals {
+  availability_zone = "${local.region}a"
+  name              = var.name
+  region            = var.region
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0"
+
+  name = local.name
+  cidr = "10.99.0.0/18"
+
+  azs              = ["${local.region}a", "${local.region}b", "${local.region}c"]
+  public_subnets   = ["10.99.0.0/24", "10.99.1.0/24", "10.99.2.0/24"]
+  private_subnets  = ["10.99.3.0/24", "10.99.4.0/24", "10.99.5.0/24"]
+
+  tags = local.tags
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 4.0"
+
+  name        = local.name
+  description = "Security group for example usage with EC2 instance"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp", "all-icmp"]
+  egress_rules        = ["all-all"]
+
+  tags = local.tags
+}
+
+
+module "ec2" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  name = local.name
+
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = var.ami_id
+  availability_zone           = local.availability_zone
+  subnet_id                   = element(module.vpc.private_subnets, 0)
+  vpc_security_group_ids      = [module.security_group.security_group_id]
+  associate_public_ip_address = true
+
+  tags = local.tags
+}
