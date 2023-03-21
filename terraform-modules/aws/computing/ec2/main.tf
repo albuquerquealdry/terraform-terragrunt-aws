b435dfa07f114f5eb096016cec6acf7d1e71b254
Aldry <albuquerquealdry@gmail.com>
2023-03-20 23:10:18 -0300

    Ajust module ssh

--- terraform-modules/aws/computing/ec2/main.tf
+++ terraform-modules/aws/computing/ec2/main.tf
@@ -7,7 +7,7 @@ module "security_group" {
   vpc_id      = var.vpc_id
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp", "all-icmp"]
+  ingress_rules       = ["http-80-tcp", "all-icmp", "ssh-tcp"]
   egress_rules        = ["all-all"]
 
   tags = var.tags
